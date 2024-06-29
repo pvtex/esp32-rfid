94a28d10a0cf34ad9a65f5237222d3438cfaeff4
PVTex <info@pvtex.de>
2024-06-29 17:37:08 +0200

    updated pin definitions in boards.js

--- src/websrc/js/boards.js
+++ src/websrc/js/boards.js
@@ -1,7 +1,17 @@
 function handleBoard() {
     var bType = parseInt(document.getElementById("boardtype").value)
     if (bType === 1) { // generic
-
+      document.getElementById("numrlys").value = "1";
+      document.getElementById("changenumrly").click();
+      document.getElementById("gpiorly"). value = "16";
+      document.getElementById("accessdeniedpin").value = "255";
+      document.getElementById("beeperpin").value = "13";
+      document.getElementById("doorstatpin").value = "14";
+      document.getElementById("ledwaitingpin").value = "12";
+      document.getElementById("openlockpin").value = "15";
+      document.getElementById("wifipin").value = "255";
+      document.getElementById("wg0pin").value = "4";
+      document.getElementById("wg1pin").value = "5";
     } else if (bType === 2) { // APWIKOGER Relay X1
       document.getElementById("numrlys").value = "1";
       document.getElementById("changenumrly").click();
